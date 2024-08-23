0261f87936490f5f15eabc70142c39f68b84a337
SameeraK <sikumarasinghe@gmail.com>
2024-08-24 07:48:51 +1200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ParentComponentnt from "./ParentComponet";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Here i am</h1>
+      <ParentComponentnt />
+    </div>
+  );
+}
+
+export default App;
